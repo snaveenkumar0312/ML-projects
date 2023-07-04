d9d283fe0906fc4647f0c36fa3b30b1e5e70279a
S Naveen Kumar <snaveenkumar0312@gmail.com>
2023-07-04 21:36:29 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from setuptools import find_packages,setup
-from typing import list
+from typing import List
+
+NAME="housing predicter",
+VERSION="0.0.1",
+AUTHOR="Naveen Kumar",
+DESCRIPTION="Complete ML project",
+PACKAGES=["housing"],
+file_path="requirements.txt"
 
 
-HYPEN_E_DOT="-e ."
-def get_requirements(file_path:str):
-    ''' it will get list of requirements'''
-    requirements=[]
+def get_requirements_list()->List[str]:
+    """
+     It will get list of requirements.
+    """
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ") for i in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-return requirements
+        return file_obj.readlines().remove("-e .")
 
 setup(
-name="ML Project"
-version="0.0.1"
-author="Naveen Kumar"
-author_email="snaveenkumar0312@gmail.com"
-package=find_packages()
-install_requires=get_requirements()
+name=NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+author_email="snaveenkumar0312@gmail.com",
+packages=PACKAGES,
+install_requires=get_requirements_list(),
 )
+
+if __name__=="__main__":
+    print(get_requirements_list())
